73138ac985295b7bb2ac384e7db116d8bcdc420f
mr-wei-zhu <x351621131@163.com>
2021-08-16 23:47:00 +0800

    登录注册

--- src/pages/personal/index.jsx
+++ src/pages/personal/index.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
-
+import './index.css'
+import { Card, WingBlank, WhiteSpace } from 'antd-mobile';
 class personal extends Component {
     render() {
         return (
-            <div>
-                <h1>personal</h1>
+            <div className='login'>
+                <a href="">登录</a>
+                <a href="">注册</a>
             </div>
         );
     }
 }
 
 export default personal;
+
+
